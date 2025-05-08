892b7d421ceb22e71fde56ee6f356b4023d3ccd1
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-05-08 23:04:31 +0900

    fix: refine descriptions of options

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_AIDER_EXTRA_ARGS =
   '--install-main-branch --model gemini/gemini-2.5-pro-exp-05-06 --edit-format diff-fenced';
-export const DEFAULT_REPOMIX_EXTRA_ARGS = '--compress --remove-empty-lines --ignore *ignore,.aider.*,.env*"';
+export const DEFAULT_REPOMIX_EXTRA_ARGS = '--compress --remove-empty-lines --include "src/**/*.{ts,tsx},**/*.md"';
